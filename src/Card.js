6d85a896b608ce663092797babe636233fad2fce
Logan Vincent <ldvsimp@gmail.com>
2021-10-15 16:23:42 -0600

    Add movie details and populate movies to page

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,17 @@ import "./Card.css";
 
 class Card extends Component {
   render() {
-    return <div id="hi">I'm a card</div>;
+    return (
+      <div className='card'>
+       <img className="card-backdrop" src={this.props.backdrop} alt="movie backdrop"/>
+       <div className="card-info">
+        <p className="card-title">{this.props.title}</p>
+        <p className="card-rating">{this.props.rating}</p>
+      </div>
+       
+   
+       </div>
+  )
   }
 }
 
