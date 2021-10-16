945d3c70775e9bf237b636b2512e9fa76761f4ce
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-10-16 12:47:05 -0400

    Display movie detail page when a card is clicked

--- src/App.js
+++ src/App.js
@@ -3,31 +3,32 @@ import Header from "./Header";
 import Feature from "./Feature";
 import Detail from "./Detail";
 import Movies from "./Movies";
-import movieData from './movieDetails.js';
+import movieData from "./movieDetails.js";
 import "./App.css";
 //Bei's comment dkfjskdlfjskdjfskjfsdkfjdsklfj
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       allMovies: movieData.movies,
-      movieIdClicked:null
-    }
+      movieClicked: null,
+    };
   }
-  handleClick = (e)=>{
-    console.log(e.target)
-    
+  handleClick = e => {
+    const forcedTarget = e.target.tagName === "ARTICLE" ? e.target : e.target.parentNode;
+    const clickedId = parseInt(forcedTarget.id);
+    const movieClicked = movieData.movies.find(movie => movie.id === clickedId);
+    return this.setState({ movieClicked: movieClicked });
+  };
 
-      return this.setState({movieIdClicked: true})
-  }
   render() {
     return (
       <div className="App">
         <Header />
-        {!this.state.movieIdClicked && <Feature />}
-        {this.state.movieIdClicked && <Detail />}
-        {!this.state.movieIdClicked && <Movies allMovies={this.state.allMovies} handleClick={this.handleClick}/>}
+        {!this.state.movieClicked && <Feature />}
+        {this.state.movieClicked && <Detail movieClicked={this.state.movieClicked} />}
+        {!this.state.movieClicked && <Movies allMovies={this.state.allMovies} handleClick={this.handleClick} />}
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Card.css";
 
 class Card extends Component {
-  // When we click on a specific card, we should be taken to 
+  // When we click on a specific card, we should be taken to
   // a view with more details on that movie.
 
   //When click happens, movie cards and feature post will be hidden, movie detail will show
@@ -11,16 +11,19 @@ class Card extends Component {
 
   render() {
     return (
-      <div className='card'>
-       <img className="card-backdrop" src={this.props.backdrop} alt="movie backdrop" onClick={event => this.props.handleClick(event)}/>
-       <div className="card-info">
-        <p className="card-title">{this.props.title}</p>
-        <p className="card-rating">{this.props.rating}</p>
-      </div>
-       
-   
-       </div>
-  )
+      <article id={this.props.id}>
+        <img
+          className="card-backdrop"
+          src={this.props.backdrop}
+          alt="movie backdrop"
+          onClick={event => this.props.handleClick(event)}
+        />
+        <div className="card-info">
+          <p className="card-title">{this.props.title}</p>
+          <p className="card-rating">{this.props.rating}</p>
+        </div>
+      </article>
+    );
   }
 }
 
