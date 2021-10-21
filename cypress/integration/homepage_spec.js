b49e030df04f6acf0d4175ad332ca574b3fd078f
Logan Vincent <ldvsimp@gmail.com>
2021-10-21 15:38:11 -0600

    Add test files and tests for homepage, detailpage and networkrequests

---
+++ cypress/integration/homepage_spec.js
@@ -0,0 +1,30 @@
+// import cy from 'cypress';
+
+describe('Homepage', () => {
+
+  beforeEach (() => {
+    cy.visit("http://localhost:3000");
+  }); 
+
+  it('User should see a header when the page is loaded', () => {
+    cy.get('header >h2')
+    .contains(' Rancid Tomatillos ')
+  });
+
+  it('User should see a login link in the header section when the page loads', () => {
+    cy.get('header >a')
+    .contains('Login')
+  });
+
+  it('User should see all movies displayed on page when it is loaded', () => {
+    cy.get('.card-holder')
+    .children()
+    .should('have.length', 40)
+  });
+
+  it('As a user, I can click a movie, and see that movie’s details', () => {
+    cy.get('article')
+    .contains("Mulan").click().url().should('include', '337401')
+  })
+
+});
