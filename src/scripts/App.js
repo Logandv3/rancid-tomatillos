6a0c993d4b66ca0e3152268a9e11f93396aec364
Logan Vincent <ldvsimp@gmail.com>
2021-10-27 19:43:51 -0600

    Add featured movie to home page

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -5,14 +5,14 @@ import Detail from "./Detail";
 import Movies from "./Movies";
 import Login from "./Login";
 import "../styles/App.css";
-import { Route, Switch } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       allMovies: [],
-      movieClicked: null,
+      featuredMovie: {}
     };
   }
 
@@ -20,8 +20,25 @@ class App extends Component {
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
       .then(response => {
         return response.json()})
-      .then(data => this.setState({ allMovies: data.movies }))
+      .then(data => this.showFeatured(data.movies) && this.setState({ allMovies: data.movies }))
       .catch(error => console.log(error));
+
+  }
+
+  showFeatured = (data) => {
+    let featured = data.reduce((arr, movie) => {
+      let rtngs = 0;
+      data.forEach((mov) => rtngs = rtngs + mov.average_rating)
+      rtngs = (rtngs/data.length) * 1.2;
+
+      if (movie.average_rating > rtngs) {
+        arr.push(movie)
+      }
+      return arr;
+    }, []);
+
+    this.setState({ featuredMovie: featured[Math.floor(Math.random() * featured.length)] });
+    return true;
   }
 
   render() {
@@ -36,8 +53,12 @@ class App extends Component {
                 <div className="header-container">
                   <Header />
                 </div>
-                  <Feature />
-                <Movies allMovies={this.state.allMovies} />
+                <div className='content-container'>
+                  <Link className='feature-link' to={`/${this.state.featuredMovie.id}`} key={this.state.featuredMovie.id}>
+                    <Feature featMov={this.state.featuredMovie} />
+                  </Link>
+                  <Movies allMovies={this.state.allMovies} />
+                </div>
               </main>
             );
           }}
